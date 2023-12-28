9845264aa205643c8ebf57b1ce9f40e8c0270b4a
aerkamk <erkamkaraburun@gmail.com>
2023-12-28 14:45:40 +0300

    little changes

--- Reflex/reflexGame.py
+++ Reflex/reflexGame.py
@@ -1,7 +1,7 @@
 import turtle
 import random
 import time
-#Test1234567
+#Test123456789
 
 def points(x, y):
     global point
--- Snake/snakeGame.py
+++ Snake/snakeGame.py
@@ -1,7 +1,7 @@
 import turtle
 import time
 import random
-#Test1234567
+#Test123456789
 
 
 speed =(0.05) 
